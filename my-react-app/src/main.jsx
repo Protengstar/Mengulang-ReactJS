825d3996ba6bebddd5bc3cf5e97e38176b427187
Protengstar <musthofasifaulq@gmail.com>
2024-03-14 10:26:53 +0700

    Membuat Card Product dengan Nested Component

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from './pages/login.jsx';
 import ErrorPage from './pages/404.jsx';
+import ProductsPage from './pages/products.jsx';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
     path: "login",
     element: <LoginPage />,
   },
+  {
+    path: "/products",
+    element: <ProductsPage />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
